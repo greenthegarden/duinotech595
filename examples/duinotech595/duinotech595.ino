f31dff4c8a22191d10fa458fe594b2011236d552
Philip Cutler <greenthegarden@gmail.com>
2016-03-01 22:25:54 +1030

    retained allArray in example

--- examples/duinotech595/duinotech595.ino
+++ examples/duinotech595/duinotech595.ino
@@ -8,16 +8,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int j = 0; j < 256; j++) {
-    updateShiftRegister(j);
-    delay(1000);
-  }
-
-  for (int i = 0; i < sizeof(pairArray); i++) {
-    updateShiftRegister(pairArray[i]);
-    delay(1000);
-  }
-
   for (int i = 0; i < sizeof(allArray); i++) {
     updateShiftRegister(allArray[i]);
     delay(1000);
