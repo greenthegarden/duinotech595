cc9019b0d9f9147e07d6e8559f9a16a67a337566
Philip Cutler <greenthegarden@gmail.com>
2016-03-04 13:11:47 +1030

    updated to fix errors

--- examples/duinotech595_pairs/duinotech595_pairs.ino
+++ examples/duinotech595_pairs/duinotech595_pairs.ino
@@ -1,6 +1,20 @@
 #include <duinotech595.h>
 
+
 Duinotech595 duinotech595(6, 5, 4);
+
+const byte LATCH_PIN = 6;
+const byte CLOCK_PIN = 5;
+const byte DATA_PIN  = 4;
+
+byte pairArray[] = {LED_PAIR_1_GREEN,
+                    LED_PAIR_1_RED,
+                    LED_PAIR_1_BLUE,
+                    LED_PAIR_2_GREEN,
+                    LED_PAIR_2_RED,
+                    LED_PAIR_2_BLUE,
+                    };
+
 
 void setup() {
   // put your setup code here, to run once:
